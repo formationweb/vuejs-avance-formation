de43466d0caf6f8d352f238cf8c820a79ee89ff1
RSamaium <sam@newbot.io>
2025-04-09 13:35:34 +0200

    Ajout d'une liste de prénoms dynamiques dans le composant Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,4 +3,14 @@
 
     <input type="text">
     <button>Rechercher</button>
+
+    <ul>
+        <li v-for="name in firstNames" :key="name">{{ name }}</li>
+    </ul>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const firstNames = ref(['ana', 'ben', 'jim'])
+</script>
